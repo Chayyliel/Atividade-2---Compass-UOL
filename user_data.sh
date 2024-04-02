a2d6a180c78ce3ba68dd119cc255564d6d8d68da
Rafael Costa <142858638+Chayyliel@users.noreply.github.com>
2024-04-01 22:46:01 -0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -13,3 +13,19 @@ mkdir /mnt/efs/
 chmod +rwx /mnt/efs/
 mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <id_do_efs>.efs.<região_de_montagem_do_efs>.amazonaws.com:/ <diretorio_de_montagem_do_nfs>
 echo "<id_do_efs>.efs.<região_de_montagem_do_efs>.amazonaws.com:/ <diretorio_de_montagem_do_nfs> nfs defaults 0 0" >> /etc/fstab
+
+echo "version: '3.8'
+services:
+  wordpress:
+    image: wordpress:lastest
+    volumes:
+      - /mnt/efs/wordpress:/var/www/html
+    ports:
+      - 80:80
+    restart: always
+    environment:
+      WORDPRESS_DB_HOST: atividade2.cpiskoe4u286.us-east-1.rds.amazonaws.com
+      WORDPRESS_DB_USER: atividade2
+      WORDPRESS_DB_PASSWORD: 123teste456
+      WORDPRESS_DB_NAME: atividade2
+      WORDPRESS_TABLE_CONFIG: wp_" | sudo tee /mnt/efs/docker-compose.yml cd /mnt/efs && sudo docker-compose up -d
