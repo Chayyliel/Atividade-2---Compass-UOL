af7142434e036d708fe3a292e3b4fbc800a60b2f
Rafael Costa <142858638+Chayyliel@users.noreply.github.com>
2024-04-03 10:50:12 -0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -8,11 +8,13 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo mkdir /mnt/efs
 sudo chmod +rwx /mnt/efs
 sudo yum install amazon-efs-utils -y
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <id-efs>.efs.us-east-1.amazonaws.com:/ mnt/efs
-sudo sh -c 'echo "<id-efs>.efs.us-east-1.amazonaws.com:/ mnt/efs nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab'
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-05f757969c22d65b4.efs.us-east-1.amazonaws.com:/ mnt/efs
+sudo sh -c 'echo "fs-05f757969c22d65b4.efs.us-east-1.amazonaws.com:/ mnt/efs nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab'
 sudo usermod -aG docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 
+cat <<EOL > /mnt/efs/docker-compose.yml
+version: '3.8'
 services:
   wordpress:
     image: wordpress:latest
@@ -22,10 +24,11 @@ services:
       - 80:80
     restart: always
     environment:
-      WORDPRESS_DB_HOST: <Endpoint DB>
-      WORDPRESS_DB_USER: <Master user DB>
-      WORDPRESS_DB_PASSWORD: <Master password DB>
-      WORDPRESS_DB_NAME: <Name DB>
-      WORDPRESS_TABLE_CONFIG: wp_ | sudo tee /mnt/efs/docker-compose.yaml
-      
-docker-compose -f /mnt/efs/docker-compose.yaml up -d
+      WORDPRESS_DB_HOST: atv2.cpiskoe4u286.us-east-1.rds.amazonaws.com
+      WORDPRESS_DB_USER: admin
+      WORDPRESS_DB_PASSWORD: teste123
+      WORDPRESS_DB_NAME: atv2
+      WORDPRESS_TABLE_CONFIG: wp_
+EOL
+
+docker-compose -f /mnt/efs/docker-compose.yml up -d
